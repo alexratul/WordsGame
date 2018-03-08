924ae7263b8d85d8c9b6b6221b1f810bc3ba051e
alexratul <alexratul12@outlook.com>
2018-03-07 23:00:52 -0600

    Start the classes for the remake of the amazon fail test, to truly solve this part of the test

---
+++ src/main/java/wordgame/wordgame.java
@@ -0,0 +1,23 @@
+package wordgame;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class wordgame {
+
+    //In this attribute the result is going to be saved.
+    List<String> resultWords;
+
+    public void wordsGameSolver(int numberOfLetters, String inputString)
+    {
+        // In this variable the result is going to be stored
+        List<String> result = new ArrayList<String>();
+
+
+        resultWords = result;
+
+    }
+
+
+
+}
